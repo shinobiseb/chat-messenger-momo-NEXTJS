b2488835e3b9e041e98c8951604915f0b19d9b28
shinobiseb <sebpatin@gmail.com>
2024-08-01 00:04:03 -0400

    styled: header, activechat component and gave props to activechatcomponent

--- components/ActiveChat.tsx
+++ components/ActiveChat.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import { chatProps } from '@/types/types'
 
-export default function ActiveChat() {
+export default function ActiveChat( {userName, profilePic, lastMessage, timeSent } : chatProps ) {
+
   return (
     <a href="/chats/1">
-      <div className='hover:bg-lightorange transition bg-orange p-2 flex flex-row m-2 rounded-lg items-center'>
+      <div className='transition bg-orange p-2 flex flex-row m-2 rounded-lg items-center'>
           <img 
-          src="https://i.pinimg.com/474x/2b/aa/60/2baa60f0bc5ff69ff16ce5b40e63e377.jpg" alt="" 
+          src={profilePic} 
+          alt="Profile Picture" 
           className='overflow-hidden rounded-full aspect-square w-14 h-14'
           />
-          <ul className='w-3/4 flex flex-col px-2'>
-              <h5     className='text-white text-xl'>
-                  User Name
-              </h5>
-              <span className='text-sm italic text-gray'>
-                  Last Message...
+          <ul className='w-5/6 flex flex-col px-2'>
+              <section className='w-full flex flex-row justify-between items-center'>
+                <h5 className='text-white text-xl'>
+                  {userName}
+                </h5>
+                <span className='text-gray text-sm'>
+                  {timeSent}
+                </span>
+              </section>
+              <span className='text-sm italic text-gray truncate'>
+                  {lastMessage}
               </span>
           </ul>
       </div>
--- components/ActiveChatList.tsx
+++ components/ActiveChatList.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import ActiveChat from './ActiveChat'
+import { FaSearch } from "react-icons/fa";
+import { HiMenu } from "react-icons/hi";
 
 function ActiveChatList() {
   return (
-    <div className='w-full h-screen flex flex-col'>
-        <header className='flex flex-row items-center justify-center w-full px-4 py-2 top-0 overflow-hidden bg-orange text-white z-10 shadow-lg'>
-            <h2 className='text-2xl'>MauChat</h2>
+    <div className='w-full h-screen flex flex-col overflow-hidden'>
+        <header className='flex flex-row items-center justify-between w-full px-4 py-2 top-0 overflow-hidden bg-orange text-white z-10 shadow-lg'>
+          <button>
+            <HiMenu size={30}/>
+          </button>
+          <h2 className='text-2xl'>MauChat</h2>
+          <button>
+            <FaSearch size={21}/>
+          </button>
         </header>
-        <ul>
-            <li>
-                <ActiveChat/>
-                <ActiveChat/>
-                <ActiveChat/>
-                <ActiveChat/>
-                <ActiveChat/>
-                <ActiveChat/>
+        <ul className='overflow-y-auto'>
+            <li className='activeChat'>
+                <ActiveChat 
+                userName='Momo' 
+                profilePic='https://pbs.twimg.com/profile_images/1555441496278843392/4KcqQrUg_400x400.jpg'
+                lastMessage='I love you so much Seb even if the world stopped being the world Id die for you'
+                timeSent='2:00'
+                />
             </li>
         </ul>
     </div>
