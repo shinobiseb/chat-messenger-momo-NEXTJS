a0515ee515482e9550ab072692f353f417e929ad
shinobiseb <sebpatin@gmail.com>
2024-08-06 03:27:19 -0400

    updated database page to be connect only

--- src/app/api/database/route.ts
+++ src/app/api/database/route.ts
@@ -3,29 +3,17 @@ import { NextResponse } from 'next/server';
 import mongoose, { ConnectOptions } from 'mongoose'
 import { MongoClient } from 'mongodb';
 
+
+
+if(!process.env.NEXT_PUBLIC_URI) {
+  throw new Error('Missing URI! Check .env or variable name')
+}
+
 const uri = process.env.NEXT_PUBLIC_URI;
 
 
-async function listMyDatabases(client: MongoClient) {
-  const databasesList = await client.db().admin().listDatabases()
-  console.log("Databases: ")
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`))
-}
-
-export async function GET() {
-  if (uri) {
-    try {
-      const client = new MongoClient(uri)
-      await client.connect()
-      await listMyDatabases(client)
-      await client.close()
-      return NextResponse.json({ message: 'Mongoose Connection Complete' });
-    } catch (error) {
-      console.error('Error:', error);
-      return NextResponse.json({ error: 'Error connecting to database' }, { status: 500 });
-    }
-  } else {
-    console.error(`You can't do anything right`);
-    return NextResponse.json({ error: `You can't do anything right` }, { status: 400 });
-  }
-}
+// async function listMyDatabases(client: MongoClient) {
+//   const databasesList = await client.db().admin().listDatabases()
+//   console.log("Databases: ")
+//   databasesList.databases.forEach(db => console.log(` - ${db.name}`))
+// }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -10,22 +10,21 @@ async function createUserCollection(client: MongoClient, userObject: Object) {
     console.log(`New Collection created with the following id: ${result.insertedId}`);
 }
 
-export async function POST(request: Request) {
-    if (uri) {
-        try {
-            const client = new MongoClient(uri);
-            await client.connect();
-            await createUserCollection(client, {
-                "userName": "Alice",
-                "chats": [],
-            });
-            await client.close();
-            return NextResponse.json({ message: 'User created successfully' }, { status: 200 });
-        } catch (error) {
-            console.error('Connection Failure: ' + error);
-            return NextResponse.json({ error: 'Error connecting to database' }, { status: 500 });
-        }
-    } else {
-        return NextResponse.json({ error: 'Database URI not set' }, { status: 500 });
-    }
+export async function POST(request: Request, response: Response) {
+    // if (uri) {
+    //     try {
+    //         const client = new MongoClient(uri);
+    //         await client.connect();
+    //         await createUserCollection(client, {
+    //             "userName": "Alice",
+    //             "chats": [],
+    //         });
+    //         await client.close();
+    //         return NextResponse.json({ message: 'User created successfully' }, { status: 200 });
+    //     } catch (error) {
+    //         console.error('Connection Failure: ' + error);
+    //         return NextResponse.json({ error: 'Error connecting to database' }, { status: 500 });
+    //     }
+    // }
+    
 }
