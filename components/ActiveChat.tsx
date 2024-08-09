d0afd1bb328a0e9705a60593d69ae43e7d70cf77
shinobiseb <sebpatin@gmail.com>
2024-08-09 12:54:40 -0400

    added more dynamic a tags

--- components/ActiveChat.tsx
+++ components/ActiveChat.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { chatProps } from '@/types/types'
+import React from 'react';
+import { useState } from 'react';
+import { chatProps } from '@/types/types';
 
-export default function ActiveChat( {userName, profilePic, lastMessage, timeSent } : chatProps ) {
-  
+export default function ActiveChat({ userName, profilePic, lastMessage, timeSent }: chatProps) {
+  const [userID, setUserID] = useState('Sketch2113131313131');
+
   return (
-    <a href="/chats/1">
-      <div className='transition bg-orange p-2 flex flex-row m-2 rounded-lg items-center'>
-          <img
-          src={profilePic} 
-          alt="Profile Picture" 
-          className='overflow-hidden rounded-full aspect-square w-14 h-14'
-          />
-          <ul className='w-5/6 sm:w-full flex flex-col px-2'>
-              <section className='w-full flex flex-row justify-between items-center'>
-                <h5 className='text-white text-xl'>
-                  {userName}
-                </h5>
-                <span className='text-gray text-sm'>
-                  {timeSent}
-                </span>
-              </section>
-              <span className='text-xs italic text-gray w-5/6 truncate'>
-                  {lastMessage}
-              </span>
-          </ul>
+    <a href={`/chats/${userID}/1`}>
+      <div className="transition bg-orange p-2 flex flex-row m-2 rounded-lg items-center">
+        <img
+          src={profilePic}
+          alt="Profile Picture"
+          className="overflow-hidden rounded-full aspect-square w-14 h-14"
+        />
+        <ul className="w-5/6 sm:w-full flex flex-col px-2">
+          <div className="w-full flex flex-row justify-between items-center">
+            <h5 className="text-white text-xl">
+              {userName}
+            </h5>
+            <span className="text-gray text-sm">
+              {timeSent}
+            </span>
+          </div>
+          <span className="text-xs italic text-gray w-5/6 truncate">
+            {lastMessage}
+          </span>
+        </ul>
       </div>
     </a>
-  )
+  );
 }
