7ba6b1bf6a7a55810633cb7b79d2106b9fad5c89
shinobiseb <sebpatin@gmail.com>
2024-08-09 12:59:57 -0400

    Added ChatWindow

--- src/app/Chats/[user]/[id]/page.tsx
+++ src/app/Chats/[user]/[id]/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import ChatWindow from '../../../../../components/ChatWindow'
+import ChatBox from '../../../../../components/ChatBox'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='bg-black'>
+      <ChatWindow/>
+    </div>
   )
 }
