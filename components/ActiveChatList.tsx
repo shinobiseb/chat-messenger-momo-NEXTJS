f08b49a3e8d228832fbdc59d3b21a87c42533a20
shinobiseb <sebpatin@gmail.com>
2024-08-10 18:41:48 -0400

    prepped for post request implementation

--- components/ActiveChatList.tsx
+++ components/ActiveChatList.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import ActiveChat from './ActiveChat'
 import { FaSearch } from "react-icons/fa";
 import { HiMenu } from "react-icons/hi";
 
 function ActiveChatList() {
+
+  const [chats, setChats] = useState([])
+
+  useEffect(()=> {
+    if(chats) {
+      console.table(chats)
+    }
+  }, [chats])
+
   return (
     <div className='w-full h-screen flex flex-col overflow-hidden'>
         <header className='flex flex-row items-center justify-between w-full px-4 py-2 top-0 overflow-hidden bg-orange text-white z-10 shadow-lg'>
@@ -16,12 +27,7 @@ function ActiveChatList() {
           </button>
         </header>
         <ul className='overflow-y-auto'>
-                <ActiveChat 
-                userName='Momo' 
-                profilePic='https://pbs.twimg.com/profile_images/1555441496278843392/4KcqQrUg_400x400.jpg'
-                lastMessage='I love you so much Seb even if the world stopped being the world Id die for you'
-                timeSent='2:00'
-                />
+
         </ul>
     </div>
   )
