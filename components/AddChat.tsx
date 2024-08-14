fdf4a3f87cbabf18332ffd6f2a05b5d4cd5ea88e
shinobiseb <sebpatin@gmail.com>
2024-08-13 20:05:43 -0400

    centered create a chat

--- components/AddChat.tsx
+++ components/AddChat.tsx
@@ -25,26 +25,26 @@ export default function AddChat(  ) {
   };
 
   return (
-    <div className='w-11/12 h-60 bg-orange rounded-md flex flex-col items-center justify-center self-center m-auto'>
-      <h3 className='text-white text-3xl mb-3 font-semibold'>
-        Create a Chat
-      </h3>
-      <form onSubmit={handleSubmit} className='flex flex-col justify-between items-center h-1/3 w-full'>
-          <input 
-          className='rounded-md px-2 py-1 w-5/6' 
-          type="text" 
-          name='userName' 
-          placeholder='User Name'
-          required
-          />
-          <input 
-          className='rounded-md bg-white py-1 px-4 mt-2' 
-          type='submit' 
-          placeholder='Create'
-          value='Create'
-          />
-      </form>
-      <button onClick={()=> history.back()} className="rounded-md bg-white py-1 px-4 mt-2">Back</button>
-    </div>
+    <div className='w-11/12 h-60 bg-orange rounded-md flex flex-col items-center justify-center absolute shadow-md z-20 left-1/2 top-1/2 transform transition-all -translate-x-1/2 -translate-y-1/2 border border-black'>
+    <h3 className='text-white text-3xl mb-3 font-semibold'>
+      Create a Chat
+    </h3>
+    <form onSubmit={handleSubmit} className='flex flex-col justify-between items-center h-1/3 w-full'>
+        <input 
+        className='rounded-md px-2 py-1 w-5/6' 
+        type="text" 
+        name='userName' 
+        placeholder='User Name'
+        required
+        />
+        <input 
+        className='rounded-md bg-white py-1 px-4 mt-2' 
+        type='submit' 
+        placeholder='Create'
+        value='Create'
+        />
+    </form>
+    <button onClick={() => history.back()} className="rounded-md bg-white py-1 px-4 mt-2">Back</button>
+  </div>
   )
 }
