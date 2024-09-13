463e3733c65291b9aabaac50993431c853e7670f
shinobiseb <sebpatin@gmail.com>
2024-09-12 23:19:11 -0400

    fixed some styling

--- components/ActiveChat.tsx
+++ components/ActiveChat.tsx
@@ -23,26 +23,15 @@ export default function ActiveChat(
   return (
     <div onClick={handleClick} className="cursor-pointer">
       <div className="transition bg-orange p-2 flex flex-row m-2 rounded-lg items-center">
-        {/* <img
-          src={profilePic}
-          alt="Profile Picture"
-          className="overflow-hidden rounded-full aspect-square w-14 h-14"
-        /> */}
-        <ul className="w-5/6 sm:w-full flex flex-col px-2">
-          <div className="w-full flex flex-row justify-between items-center">
+        <ul className="w-full sm:w-full flex flex-col px-2">
+          <li className="w-full flex flex-row justify-between items-center">
             <h5 className="text-white text-xl">
               {targetUserName}
             </h5>
-            {/* <span className="text-gray text-sm">
-              {time}
-            </span> */}
-          </div>
-          {/* <span className="text-xs italic text-gray w-5/6 truncate">
-            {lastMessage}
-          </span> */}
-          <span>
-            <MdMessage />
-          </span>
+            <span>
+              <MdMessage size={30} fill='white'/>
+            </span>
+          </li>
         </ul>
       </div>
     </div>
