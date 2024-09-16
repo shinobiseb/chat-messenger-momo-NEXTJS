e3c81a383f7993219f2045e9b058c310eb218482
shinobiseb <sebpatin@gmail.com>
2024-09-16 16:07:16 -0400

    useRouter instead of a tags

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,23 +1,28 @@
+import { useRouter } from "next/navigation";
+
 export default function Welcome() {
+
+  const router = useRouter()
 
   return (
     <div className="bg-orange w-11/12 max-w-xl h-60 rounded-xl flex flex-col justify-around items-center py-4 drop-shadow-md">
       <h2 className="text-white text-4xl uppercase font-semibold mt-4"> MauChat!</h2>
       <span className="text-lightgray italic">V0.1 - Alpha</span>
       <section className='welcome-items h-2/3 w-5/6 flex flex-col items-center justify-evenly'>
-        <a
+        <button
           id="SignInButton"
           className="bg-white button-hover rounded-xl text-lg font-semibold px-4 p-2 flex flex-row justify-center overflow-hidden items-center drop-shadow-md"
-          href='/signin'
+          onClick={()=>router.push('/SignIn')}
         >
           <p>Sign In</p>
-        </a>
-        <a
+        </button>
+        <button
           id="SignInButton"
           className="bg-white button-hover rounded-xl text-lg font-semibold px-4 p-2 flex flex-row justify-center items-center drop-shadow-md text-center"
-        href='/signup'>
+          onClick={()=>router.push('/SignUp')}
+          >
           Sign Up
-        </a>
+        </button>
       </section>
     </div>
   );
