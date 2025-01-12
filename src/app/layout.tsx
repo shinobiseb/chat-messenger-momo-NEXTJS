940f7e26e82519526eb01de53b5200e6c91ce3d2
Sebastian Patin <sebpatin@gmail.com>
2025-01-11 20:57:38 -0500

    no way

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <UserStateProvider>
-      <html lang="en" className="bg-background-svg bg-cover">
+      <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
     </UserStateProvider>
