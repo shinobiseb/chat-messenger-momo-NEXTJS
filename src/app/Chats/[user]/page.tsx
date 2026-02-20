3d139f1a092ba3bec4820f96d2d2da673804e8e8
shinobiseb <sebpatin@gmail.com>
2026-02-20 15:44:44 -0500

    auth works

--- src/app/Chats/[user]/page.tsx
+++ src/app/Chats/[user]/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { useState, useEffect } from 'react';
 import ActiveChatList from '../../../../components/ActiveChatList';
 import { useRouter } from 'next/navigation';
@@ -17,7 +15,7 @@ export default function Page() {
 
   return (
     <div className=''>
-
+      
     </div>
   );
 }
--- src/app/Chats/page.tsx
+++ src/app/Chats/page.tsx
@@ -5,7 +5,6 @@ import { redirect } from "next/navigation";
 export default async function RedirectToUserChat() {
   const session = await auth();
 
-  // 1. If not logged in, boot them to the home page
   if (!session?.user) {
     redirect("/");
   }
@@ -15,4 +14,19 @@ export default async function RedirectToUserChat() {
     : session.user.id;
 
   redirect(`/chats/${userSlug}`);
+
+    //Schema for session
+    //   {
+    //   user: {
+    //     id: '699362920ab017fad7bc3fd5',
+    //     name: 'Ney Sebastian Patin',
+    //     email: 'sebpatin@gmail.com',
+    //     image: 'https://lh3.googleusercontent.com/a/ACg8ocLE23q-ZmxIX7mKDsUTP0kBJg3ry4THXt85VAaFm5yI-N8ExMnvEg=s96-c',
+    //     emailVerified: null
+    //   },
+    //   id: '699362930ab017fad7bc3fd7',
+    //   sessionToken: 'b8e8e591-8230-4af2-a2d4-f483632ad8dd',
+    //   userId: '699362920ab017fad7bc3fd5',
+    //   expires: '2026-03-22T19:08:10.326Z'
+    // }
 }
